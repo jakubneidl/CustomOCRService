5f51c8f0cbaa8b75542f42e7fdce06373cb1942b
Jakub Neidl <jakub.neidl@gmail.com>
2023-04-08 21:33:30 +0200

    update

--- src/main/java/com/example/customocrservice/config/TesseractConfig.java
+++ src/main/java/com/example/customocrservice/config/TesseractConfig.java
@@ -1,36 +1,39 @@
 package com.example.customocrservice.config;
 
 import com.example.customocrservice.model.ocr.LanguageEnum;
+import lombok.SneakyThrows;
 import net.sourceforge.tess4j.Tesseract;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.context.WebApplicationContext;
 
 import java.io.File;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 @Configuration
 public class TesseractConfig {
 
     private static String TESSERACT_TRAINED_DATA_PATH = "tessdata";
 
+    @SneakyThrows
     @Bean
     public Tesseract tesseract() {
         Tesseract tesseract = new Tesseract();
-        try {
-            File tessDataFolder = new File(getClass().getClassLoader().getResource(TESSERACT_TRAINED_DATA_PATH).toURI());
-            if (tessDataFolder.exists()) {
-                tesseract.setDatapath(tessDataFolder.getAbsolutePath());
-            } else {
-                throw new RuntimeException("Failed to find Tesseract trained data on classpath");
-            }
-        } catch (URISyntaxException e) {
-            throw new RuntimeException("Failed to convert URL to URI for Tesseract trained data", e);
-        }
-        tesseract.setLanguage(LanguageEnum.CES.getLangCode());
+
+        Resource tessDataResource = new ClassPathResource("tessdata");
+        Path tempTessDataFolder = Files.createTempDirectory("tessdata");
+        FileCopyUtils.copy(tessDataResource.getFile(), tempTessDataFolder.toFile());
+
+        tesseract.setDatapath(tempTessDataFolder.toString());
+        tesseract.setLanguage("eng");
         return tesseract;
     }
 }
