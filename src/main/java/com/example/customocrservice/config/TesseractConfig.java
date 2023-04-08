df3d86097ba3a91ac4c050f93ed918fd28cd3edd
Jakub Neidl <jakub.neidl@gmail.com>
2023-04-08 21:05:19 +0200

    update tessdata location

--- src/main/java/com/example/customocrservice/config/TesseractConfig.java
+++ src/main/java/com/example/customocrservice/config/TesseractConfig.java
@@ -8,21 +8,27 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.io.File;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 @Configuration
 public class TesseractConfig {
 
-    private static String TESSERACT_TRAINED_DATA_PATH = "/tessdata";
+    private static String TESSERACT_TRAINED_DATA_PATH = "tessdata";
 
     @Bean
     public Tesseract tesseract() {
         Tesseract tesseract = new Tesseract();
-        URL resource = getClass().getResource(TESSERACT_TRAINED_DATA_PATH);
-        if (resource != null) {
-            tesseract.setDatapath(resource.getPath());
-        } else {
-            throw new RuntimeException("Failed to find Tesseract trained data on classpath");
+        try {
+            File tessDataFolder = new File(getClass().getClassLoader().getResource(TESSERACT_TRAINED_DATA_PATH).toURI());
+            if (tessDataFolder.exists()) {
+                tesseract.setDatapath(tessDataFolder.getAbsolutePath());
+            } else {
+                throw new RuntimeException("Failed to find Tesseract trained data on classpath");
+            }
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("Failed to convert URL to URI for Tesseract trained data", e);
         }
         tesseract.setLanguage(LanguageEnum.CES.getLangCode());
         return tesseract;
