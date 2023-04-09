4aa92e7a5273e65d3114b82fcc5f5085eaf7f4a5
Jakub Neidl <jakub.neidl@gmail.com>
2023-04-09 13:45:07 +0200

    add exception handler

---
+++ src/main/java/com/example/customocrservice/controller/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.customocrservice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        String errorMessage = "An error occurred: " + ex.getMessage();
+        return new ResponseEntity<>(errorMessage, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
